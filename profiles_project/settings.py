feb9dcdbeb1cc02e99212f2a8ab6fce6288a9599
vinayshashank <vinay9023@gmail.com>
2022-01-23 11:56:23 -0500

    Added customer user model, manager and migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@a7(u!_di_%k&q)z5jq0kdgmiu0iibc#y#fxe7j$z+lbxk%7#4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
